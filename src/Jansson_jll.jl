79fd654c3180d76ba559849726f2ed67a383f988
jlbuild <juliabuildbot@gmail.com>
2021-02-09 03:04:39 +0000

    Jansson_jll build 2.13.1+0

---
+++ src/Jansson_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule Jansson_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("Jansson")
+JLLWrappers.@generate_main_file("Jansson", UUID("83cbd138-b029-500a-bd82-26ec0fbaa0df"))
+end  # module Jansson_jll
